b7869565f018ead45177e025ae8ec48bb97e07f7
Junho Lee <junlee5536@gmail.com>
2019-05-06 13:15:10 +0900

    Create time_calculator.c

---
+++ time_calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//distance = rate * time
+
+int main() {
+	//declaring variables
+	int distance, kmh, hour, minute;
+	double second, res1, res2;
+
+	printf("Enter the distance (in kilometers) : ");
+	scanf("%d", &distance);
+	printf("\n");
+	printf("Enter the desired speed (in kilometer per hour) : ");
+	scanf("%d", &kmh);
+	printf("\n");
+
+	//calculating time 
+	hour = distance / kmh;
+	res1 = (double) distance / (double) kmh;
+	res2 = (res1 - hour) * 60;
+	minute = (int) res2;
+	second = (res2 - minute) * 60;
+
+	//printing result
+	printf("Estimated duration of travel : %d hour(s) %d minute(s) %.3lf second(s)\n", hour, minute, second);
+
+	return 0;
+}
