cb710608b6495d66ed4d24c0dc18961cc1e80c93
Junho Lee <junlee5536@gmail.com>
2019-05-21 21:14:24 +0900

    Update tuition_calculator.c

--- tuition_calculator.c
+++ tuition_calculator.c
@@ -19,12 +19,12 @@ int main() {
 		printf("\n");
 
 		//calculating scholarship amount
-		if (1) {
+		if (gpa >= 3.0 || wealth <= 20) {
 			if (gpa >= 4.0) {
 				if (wealth > 100) {
 					tuition_new = tuition_old * 0.8;
 				}
-				else if (wealth < 100) {
+				else if (wealth <= 100) {
 					tuition_new = tuition_old * 0.6;
 				}
 			}
@@ -32,17 +32,19 @@ int main() {
 				if (wealth > 100) {
 					tuition_new = tuition_old * 0.9;
 				}
-				else if (wealth < 100) {
+				else if (wealth <= 100) {
 					tuition_new = tuition_old * 0.7;
 				}
 			}
 			else if (wealth <= 20) {
 				tuition_new = tuition_old * 0.3;
 			}
+
 			printf("You have a $ %d scholarship\n", tuition_old - tuition_new);
 			printf("Your tuition before the scholarship : $ %d\n", tuition_old);
 			printf("Your tuition after the scholarship : $ %d\n\n", tuition_new);
 		}
+
 		else {
 			tuition_new = tuition_old;
 			printf("We are sorry, but you do not qualify for a scholarship\n", tuition_old);
