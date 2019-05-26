487b2a341b902ffd1e336348b86be5f5d1542442
Junho Lee <junlee5536@gmail.com>
2019-05-27 08:48:38 +0900

    Update time_calculator.c

--- time_calculator.c
+++ time_calculator.c
@@ -20,6 +20,9 @@ int main() {
 	res2 = (res1 - hour) * 60;
 	minute = (int) res2;
 	second = (res2 - minute) * 60;
+	//shorter version
+	//minute = (int) (((double)distance / (double)kmh) - hour) * 60;
+	//second = (((((double)distance / (double)kmh) - hour) * 60) - minute) * 60;
 
 	//printing result
 	printf("Estimated duration of travel : %d hour(s) %d minute(s) %.3lf second(s)\n", hour, minute, second);
